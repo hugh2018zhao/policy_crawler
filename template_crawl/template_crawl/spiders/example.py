035501d6ef771a7cc7e579c287bdb2f93654ba49
BuGoNee <luyang911@gmail.com>
2021-03-27 22:29:07 +0800

    更新template中关于deltafetch的信息

--- template_crawl/template_crawl/spiders/example.py
+++ template_crawl/template_crawl/spiders/example.py
@@ -5,6 +5,7 @@ class ExampleSpider(scrapy.Spider):
     name = 'example'
     allowed_domains = ['example.com']
     start_urls = ['http://example.com/']
+
     custom_settings = {
         'DOWNLOAD_DELAY': 0.04,  # 请求间隔时间，提升该数字可降低爬取频率
         'TELNETCONSOLE_PORT': [6245],  # telnet端口号
@@ -18,18 +19,16 @@ class ExampleSpider(scrapy.Spider):
             'policy_crawler_common.scrapy_extensions.middlewares.ua_rotate.RandomUserAgent': 1,
             'policy_crawler_common.scrapy_extensions.middlewares.abu_proxy.AbuProxyMiddleware': 901,
         },
-        'USER_AGENT_ROTATE_ENABLED': False,
+        'USER_AGENT_ROTATE_ENABLED': False,  # 开启RandomUserAgent middleware
         'USER_AGENT_TYPE': 'MOBILE',
         'USER_AGENT': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/47.0.2526.73 Chrome/47.0.2526.73 Safari/537.36',
-        'PROXY_ENABLED': False,
+        'PROXY_ENABLED': False,  # AbuProxyMiddleware的开关
 
-        # 'SPIDER_MIDDLEWARES': {
-        # BOT_NAME+'.middlewares.deltafetch.DeltaFetchMiddleware': 901,
-        # },
-        # 'DELTAFETCH_ENABLED': True,
-        # 'DELTAFETCH_KEY_NAME': 'id',
-        # 'DELTAFETCH_DB_NAME': 'crawl',
-        # 'DELTAFETCH_TABLE_NAME': 'base',
+        'SPIDER_MIDDLEWARES': {
+            'policy_crawler_common.scrapy_extensions.middlewares.deltafetch.DeltaFetchMiddleware': 901,
+        },
+        'DELTAFETCH_ENABLED': False,
+        'DELTAFETCH_KEY_NAME': 'raw_key',
         'ITEM_PIPELINES': {
             'policy_crawler_common.scrapy_extensions.pipelines.MysqlExportPipeline': 300,
             'crawlab.pipelines.CrawlabMongoPipeline': 888,
@@ -39,13 +38,13 @@ class ExampleSpider(scrapy.Spider):
         'COOKIES_ENABLED': False,
 
         # 分布式爬虫，切换调度器
-        "SCHEDULER": "scrapy_redis.scheduler.Scheduler",
+        # "SCHEDULER": "scrapy_redis.scheduler.Scheduler",
         # 分布式爬虫，切换爬虫请求的去重策略
-        "DUPEFILTER_CLASS": "scrapy_redis.dupefilter.RFPDupeFilter",
+        # "DUPEFILTER_CLASS": "scrapy_redis.dupefilter.RFPDupeFilter",
         # 分布式爬虫，队列持久化，即爬虫结束时是否清理队列
-        "SCHEDULER_PERSIST": False,
+        # "SCHEDULER_PERSIST": False,
         # 分布式爬虫: 爬虫启动后先等待30秒时间，用于往redis中的请求队列填充req对象，若该队列为空则爬虫会结束
-        "SCHEDULER_IDLE_BEFORE_CLOSE": 30,
+        # "SCHEDULER_IDLE_BEFORE_CLOSE": 30,
     }
 
     def parse(self, response):
